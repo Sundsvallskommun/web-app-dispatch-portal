7239dfaa4df313e5106e0ae62218ec7d47b5f376
Emil Högberg <emil.hogberg@sundsvall.se>
2025-01-29 08:44:46 +0100

    Created a new sidebar with a new button in header.

--- frontend/src/components/main-menu/main-menu.component.tsx
+++ frontend/src/components/main-menu/main-menu.component.tsx
@@ -1,15 +1,19 @@
-import { MenuBar } from '@sk-web-gui/react';
+import { MenuBar, Button } from '@sk-web-gui/react';
 import NextLink from 'next/link';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { mainMenuItems } from './main-menu-items';
+import { HelpComposer } from '../help/help-composer';
 
 export const MainMenu: React.FC = () => {
   const [active, setActive] = useState<number>(0);
+  const [showHelpComposer, setShowHelpComposer] = useState(false);
 
+  const openHelpComposer = () => setShowHelpComposer(true);
+  const closeHelpComposer = () => setShowHelpComposer(false);
   useEffect(() => {
-    if(window.location.pathname === '/statistics') {
+    if (window.location.pathname === '/statistics') {
       setActive(1);
-    } else if(window.location.pathname === '/help') {
+    } else if (window.location.pathname === '/help') {
       setActive(2);
     } else {
       setActive(0);
@@ -24,6 +28,12 @@ export const MainMenu: React.FC = () => {
             <a>{item.label}</a>
           </MenuBar.Item>
         ))}
+        <div>
+          <Button inverted="true" onClick={openHelpComposer}>
+            Hjälp
+          </Button>
+          <HelpComposer show={showHelpComposer} closeHandler={closeHelpComposer} />
+        </div>
       </MenuBar>
     </div>
   );
