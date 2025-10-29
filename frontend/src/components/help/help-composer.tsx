e36cd82372cfad2e579d1970243cfe95416b10db
Abed Naqeeb <abed.naqeeb@sundsvall.se>
2025-10-29 10:33:47 +0100

    fix: made the help composer header font size as per figma.

--- frontend/src/components/help/help-composer.tsx
+++ frontend/src/components/help/help-composer.tsx
@@ -20,7 +20,7 @@ export const HelpComposer: React.FC<IHelpComposerProps> = ({ show, helpType, clo
   return (
     <HelpWrapper label="Hjälp" closeHandler={closeHandler} show={show}>
       <div className="my-md py-8 px-40 flex flex-col gap-12 ">
-        <h1>{helpHeader}</h1>
+        <div className="text-h4-lg font-header">{helpHeader}</div>
         <Help filterTag={helpType} />
       </div>
     </HelpWrapper>
