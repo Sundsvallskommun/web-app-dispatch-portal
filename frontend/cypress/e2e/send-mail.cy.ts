e4dcc328dbebbe1ed3bf287d3fb4917d1436491f
SUKMTobNorx <tobias.nordin@sundsvall.se>
2025-10-30 14:29:46 +0100

    feat: refactors test

--- frontend/cypress/e2e/send-mail.cy.ts
+++ frontend/cypress/e2e/send-mail.cy.ts
@@ -11,7 +11,35 @@ describe('Send mail flow', () => {
     cy.visit('/send/mail');
   });
 
+  describe('Recipient handler', () => {
+    it('should add an correct personal number in the search input, show dialog and add a person on enter', () => {
+      addRecipient('189001019802');
+    });
+
+    it('should remove an added person', () => {
+      addRecipient('189001019802');
+      cy.get('[data-cy="recipient-table"]').should('exist');
+      cy.get('[data-cy="recipient-table"]').find('[data-cy="delete-person-button"]').first().click();
+      cy.get('[data-cy="recipient-table"]').should('not.exist');
+    });
+
+    it('should add persons from csv', () => {
+      cy.get('input[type="radio"][value="1"]').check();
+      cy.get('[data-cy="file-input"]').selectFile('cypress/files/personal-numbers.csv', { force: true });
+      cy.wait('@recipients');
+      cy.get('[data-cy="recipients"]').contains('personal-numbers.csv').should('be.visible');
+    });
+
+    it('should navigate to next step if a recipient is added and "next" is clicked', () => {
+      navigateToFilesStep('189001019802');
+      cy.get('.sk-progress-stepper-step[data-progress="current"]').should('exist').and('contain.text', 'Filer');
+    });
+  });
+
   describe('Attachment handler', () => {
+    beforeEach(() => {
+      navigateToFilesStep('189001019802');
+    });
     it('should add two documents', () => {
       cy.get('[data-cy="file-input"]').selectFile('cypress/files/document1.pdf', { force: true });
       cy.get('[data-cy="attachments"]').contains('document1.pdf').should('be.visible');
@@ -39,49 +67,19 @@ describe('Send mail flow', () => {
 
     it('should navigate to next step if a file is added and "next" is clicked', () => {
       cy.get('[data-cy="file-input"]').selectFile('cypress/files/document1.pdf', { force: true });
-      cy.get('[data-cy="next-button"]').click();
-      cy.get('.sk-progress-stepper-step[data-progress="current"]').should('exist').and('contain.text', 'Lägg till');
-    });
-  });
-
-  describe('Recipient handler', () => {
-    beforeEach(() => {
-      cy.get('[data-cy="file-input"]').selectFile('cypress/files/document1.pdf', { force: true });
-      cy.get('[data-cy="next-button"]').click();
-    });
-
-    it('should add an correct personal number in the search input, show dialog and add a person on enter', () => {
-      addPerson('189001019802');
-    });
-
-    it('should remove an added person', () => {
-      addPerson('189001019802');
-      cy.get('[data-cy="recipient-table"]').should('exist');
-      cy.get('[data-cy="recipient-table"]').find('[data-cy="delete-person-button"]').first().click();
-      cy.get('[data-cy="recipient-table"]').should('not.exist');
-    });
-
-    it('should add persons from csv', () => {
-      cy.get('input[type="radio"][value="1"]').check();
-      cy.get('[data-cy="file-input"]').selectFile('cypress/files/personal-numbers.csv', { force: true });
-      cy.wait('@recipients');
-      cy.get('[data-cy="recipients"]').contains('personal-numbers.csv').should('be.visible');
-    });
-
-    it('should navigate to next step if a recipient is added and "next" is clicked', () => {
-      addPerson('189001019802');
+      cy.get('[data-cy="attachments"]').should('exist');
       cy.get('[data-cy="next-button"]').click();
       cy.get('.sk-progress-stepper-step[data-progress="current"]')
         .should('exist')
-        .and('contain.text', 'Ange avsändare');
+        .and('contain.text', 'Rubrik och förvaltning');
     });
   });
 
   describe('Sender handler', () => {
     beforeEach(() => {
-      cy.get('[data-cy="file-input"]').selectFile('cypress/files/document1.pdf', { force: true });
+      addRecipient('189001019802');
       cy.get('[data-cy="next-button"]').click();
-      addPerson('189001019802');
+      cy.get('[data-cy="file-input"]').selectFile('cypress/files/document1.pdf', { force: true });
       cy.get('[data-cy="next-button"]').click();
     });
 
@@ -92,10 +90,17 @@ describe('Send mail flow', () => {
   });
 });
 
-const addPerson = (personNumber: string) => {
+const addRecipient = (personNumber: string) => {
   cy.get('[data-cy="person-search-field"]').should('exist');
   cy.get('[data-cy="person-search-field"]').type(personNumber, { force: true });
   cy.wait('@recipient');
   cy.get('[data-cy="preview-person"]').should('be.visible');
   cy.get('[data-cy="person-search-field"]').type('{enter}', { force: true });
+};
+
+const navigateToFilesStep = (personNumber: string) => {
+  addRecipient(personNumber);
+  cy.get('[data-cy="recipient-table"]').should('exist');
+  cy.get('[data-cy="recipient-table"] tbody tr').should('have.length.gte', 1);
+  cy.get('[data-cy="next-button"]').click();
 };
