55b3b1ab098bb76050d4e4d29f3958d27a1e10be
SUKMTobNorx <tobias.nordin@sundsvall.se>
2025-10-31 10:12:37 +0100

    feat: updates test

--- frontend/cypress/e2e/send-mail.cy.ts
+++ frontend/cypress/e2e/send-mail.cy.ts
@@ -30,13 +30,14 @@ pages.map((p) => {
     });
 
     describe('Recipient handler', () => {
+      const eligiblePn = personalNumber.isEligible;
       if (p.route === '/send/mail') {
         it('should add an correct personal number in the search input, show dialog and add a person on enter', () => {
-          addRecipient(personalNumber.isNotEligible, true, false);
+          addRecipient(personalNumber.isNotEligible, true);
         });
 
         it('should remove an added person', () => {
-          addRecipient(personalNumber.isNotEligible, true, false);
+          addRecipient(personalNumber.isNotEligible, true);
           cy.get('[data-cy="recipient-table"]').should('exist');
           cy.get('[data-cy="recipient-table"]').find('[data-cy="delete-person-button"]').first().click();
           cy.get('[data-cy="recipient-table"]').should('not.exist');
@@ -48,36 +49,38 @@ pages.map((p) => {
           cy.wait('@recipients');
           cy.get('[data-cy="recipients"]').contains('personal-numbers.csv').should('be.visible');
         });
-
-        it('should navigate to next step if a recipient is added and "next" is clicked', () => {
-          navigateToAttachmentHandler(personalNumber.isNotEligible, false);
-          cy.get('.sk-progress-stepper-step[data-progress="current"]').should('exist').and('contain.text', 'Filer');
-        });
       } else {
         it('should show validation error if test person does not have Kivra', () => {
-          addRecipient(personalNumber.isNotEligible, false, true);
+          addRecipient(personalNumber.isNotEligible, false);
           cy.get('[data-cy="preview-person"]').should('contain.text', 'Har inte digital brevlåda hos Kivra');
         });
 
         it('should show success message if test person has Kivra', () => {
-          const pn = personalNumber.isEligible;
-          if (pn === personalNumber.isEligible) {
-            cy.intercept('POST', '**/eligibility-kivra', { fixture: 'kivra.json' }).as('kivra-check');
-          }
-          addRecipient(pn, false, true);
+          addAndCheckEligibility(eligiblePn);
           cy.get('[data-cy="preview-person"]').should('contain.text', 'Har digital brevlåda hos Kivra');
         });
+
+        it('should remove the added person', () => {
+          addAndCheckEligibility(eligiblePn);
+          cy.get('[data-cy="person-search-field"]').type('{enter}', { force: true });
+          cy.get('[data-cy="recipient-table"]').should('exist');
+          cy.get('[data-cy="recipient-table"]').find('[data-cy="delete-person-button"]').first().click();
+          cy.get('[data-cy="recipient-table"]').should('not.exist');
+        });
       }
+
+      it('should navigate to next step if a recipient is added and "next" is clicked', () => {
+        if (p.route === '/send/rek-mail') addAndCheckEligibility(eligiblePn);
+        navigateToAttachmentHandler(personalNumber.isNotEligible);
+        cy.get('.sk-progress-stepper-step[data-progress="current"]').should('exist').and('contain.text', 'Filer');
+      });
     });
 
     describe('Attachment handler', () => {
       beforeEach(() => {
         const isRecommended = p.route === '/send/rek-mail';
         cy.intercept('POST', '**/eligibility-kivra', { fixture: 'kivra.json' }).as('kivra-check');
-        navigateToAttachmentHandler(
-          isRecommended ? personalNumber.isEligible : personalNumber.isNotEligible,
-          isRecommended
-        );
+        navigateToAttachmentHandler(isRecommended ? personalNumber.isEligible : personalNumber.isNotEligible);
       });
 
       it('should add two documents', () => {
@@ -118,7 +121,7 @@ pages.map((p) => {
     describe('Sender handler', () => {
       beforeEach(() => {
         cy.intercept('POST', '**/eligibility-kivra', { fixture: 'kivra.json' }).as('kivra-check');
-        addRecipient(personalNumber.isEligible, true, p.route === '/send/rek-mail');
+        addRecipient(personalNumber.isEligible, true);
         cy.get('[data-cy="next-button"]').click();
         cy.get('[data-cy="file-input"]').selectFile('cypress/files/document1.pdf', { force: true });
         cy.get('[data-cy="next-button"]').click();
@@ -132,7 +135,7 @@ pages.map((p) => {
   });
 });
 
-const addRecipient = (personNumber: string, enter: boolean, isRecommended: boolean) => {
+const addRecipient = (personNumber: string, enter: boolean) => {
   cy.get('[data-cy="person-search-field"]').should('exist');
   cy.get('[data-cy="person-search-field"]').type(personNumber, { force: true });
   cy.wait('@recipient');
@@ -140,9 +143,16 @@ const addRecipient = (personNumber: string, enter: boolean, isRecommended: boole
   if (enter) cy.get('[data-cy="person-search-field"]').type('{enter}', { force: true });
 };
 
-const navigateToAttachmentHandler = (personNumber: string, isRecommended: boolean) => {
-  addRecipient(personNumber, true, isRecommended);
+const navigateToAttachmentHandler = (personNumber: string) => {
+  addRecipient(personNumber, true);
   cy.get('[data-cy="recipient-table"]').should('exist');
   cy.get('[data-cy="recipient-table"] tbody tr').should('have.length.gte', 1);
   cy.get('[data-cy="next-button"]').click();
+};
+
+const addAndCheckEligibility = (pn: PersonNumber['isEligible']) => {
+  if (pn === personalNumber.isEligible) {
+    cy.intercept('POST', '**/eligibility-kivra', { fixture: 'kivra.json' }).as('kivra-check');
+  }
+  addRecipient(pn, false);
 };
