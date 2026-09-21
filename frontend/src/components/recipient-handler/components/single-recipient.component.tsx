82c8d0e12fa10ff2400344dd8bbe4fd6c2231f4c
Sandra Karlberg <sandra.karlberg@hotmail.com>
2026-09-21 14:00:41 +0200

    fix: allows signatories regardless of delivery method

--- frontend/src/components/recipient-handler/components/single-recipient.component.tsx
+++ frontend/src/components/recipient-handler/components/single-recipient.component.tsx
@@ -42,6 +42,7 @@ export const SingleRecipient: React.FC<SingleRecipientProps> = ({
 
   const isRek = sendType === formSendType.REK_MAIL;
   const isPersonOnly = isRek || sendType === formSendType.ESIGNING;
+  const ignoresDeliveryMethod = sendType === formSendType.ESIGNING;
   const emailIsValid = emailPattern.test(email.trim());
   const canSubmit = !requireEmail || emailIsValid;
 
@@ -108,7 +109,7 @@ export const SingleRecipient: React.FC<SingleRecipientProps> = ({
       }
       return;
     }
-    if (foundRecipient?.deliveryMethod === 'DELIVERY_NOT_POSSIBLE' || !canSubmit) return;
+    if ((!ignoresDeliveryMethod && foundRecipient?.deliveryMethod === 'DELIVERY_NOT_POSSIBLE') || !canSubmit) return;
 
     if (onAdd) {
       onAdd(foundRecipient, email.trim());
